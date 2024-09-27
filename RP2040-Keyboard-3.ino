d84ff085933ac1e6e9807d05b26aad807a9f6c15
snet.at <dev@snet.at>
2024-09-27 17:06:35 +0200

    Update RP2040-Keyboard-3.ino

--- RP2040-Keyboard-3.ino
+++ RP2040-Keyboard-3.ino
@@ -23,18 +23,6 @@
   This example is in the public domain.
 
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/KeyboardLogout
-
-  ---
-
-  https://www.waveshare.com/wiki/RP2040-Keyboard-3#Pico_Get_Start_Quick
-  https://www.zfrontier.com/lab/keyboardTester
-
-  These macro definitions can be found in the HID_Keyboard.h file.
-  Click Ctrl-F12 on #include "Keyboard.h" -> #include <HID_Keyboard.h>
-
-  Seitch to Drive Mode "sudo stty -F /dev/ttyACM0 1200"
-  cp /tmp/arduino/sketches/xxxxxxxx/RP2040-Keyboard-3.ino.uf2
-
 */
 
 #define OSX 0
